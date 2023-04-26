d5a88a2b50e8497ea1eca76ddfd4f3d78a91b1c9
Jamey <jameyschaap06@gmail.com>
2023-04-26 13:02:19 +0200

    Updated LoginPage's loading CSS

--- src/SpartanFitness.ClientApp/src/pages/Exercises.tsx
+++ src/SpartanFitness.ClientApp/src/pages/Exercises.tsx
@@ -72,7 +72,7 @@ const ExercisesPage = () => {
               ))}
           </div>
 
-          {!(isLoading == false) && (
+          {(isLoading == true || isLoading == undefined) && (
             <div
               role="status"
               className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
