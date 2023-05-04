8389027b4f0a4a703b03ffc687c3ada963d8bfc6
Jamey <jameyschaap06@gmail.com>
2023-05-04 20:40:51 +0200

    Added GetMusclePageByMuscleGroupQuery

---
+++ src/SpartanFitness.Application/Muscles/Query/GetMusclePageByMuscleGroup/GetMusclePageByMuscleGroupQueryValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace SpartanFitness.Application.Muscles.Query.GetMusclePageByMuscleGroup;
+
+public class GetMusclePageByMuscleGroupQueryValidator : AbstractValidator<GetMusclePageByMuscleGroupQuery>
+{
+  public GetMusclePageByMuscleGroupQueryValidator()
+  {
+    RuleFor(x => x.MuscleGroupId)
+      .Must(id => Guid.TryParse(id, out _))
+      .WithMessage("The muscle group ID must be a valid GUID");
+
+    RuleFor(x => x.PageNumber)
+      .Must(x => x == null || x > 0)
+      .WithMessage("The page number must be bigger than 0");
+
+    RuleFor(x => x.PageSize)
+      .Must(x => x == null || x > 0)
+      .WithMessage("The page size must be bigger than 0");
+  }
+}
